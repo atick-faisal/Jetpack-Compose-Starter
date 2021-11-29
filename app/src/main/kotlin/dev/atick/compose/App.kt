23087348b363a9bd0ccb483d06bd9548b03eb070
Atick Faisal <atickfaisal@gmail.com>
2021-11-29 18:16:26 +0600

    Initialize dagger hilt

---
+++ app/src/main/kotlin/dev/atick/compose/App.kt
@@ -0,0 +1,7 @@
+package dev.atick.compose
+
+import android.app.Application
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class App : Application()
--- app/src/main/kotlin/dev/atick/compose/MainActivity.kt
+++ app/src/main/kotlin/dev/atick/compose/MainActivity.kt
@@ -8,8 +8,10 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
 import dev.atick.compose.ui.theme.JetpackComposeStarterTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
