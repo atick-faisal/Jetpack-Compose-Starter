1e0a0a3ac7b541c65ea1c948b469c46bb9d03971
Atick Faisal <atickfaisal@gmail.com>
2022-01-27 23:46:04 +0600

    level up extensions

--- core/src/main/kotlin/dev/atick/core/utils/extensions/ContextExtensions.kt
+++ core/src/main/kotlin/dev/atick/core/utils/extensions/ContextExtensions.kt
@@ -2,7 +2,14 @@ package dev.atick.core.utils.extensions
 
 import android.content.Context
 import android.widget.Toast
+import dev.atick.core.BuildConfig
 
 fun Context.showToast(message: String) {
     Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+}
+
+fun Context.debugMessage(error: String) {
+    if (BuildConfig.DEBUG) {
+        this.showToast(error)
+    }
 }
