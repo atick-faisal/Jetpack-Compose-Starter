990b62e6079cd7af6b0e318ce3618627613e4660
Atick Faisal <atickfaisal@gmail.com>
2023-02-08 18:50:30 +0300

    Add UiText utility

--- app/src/main/kotlin/dev/atick/compose/ui/home/HomeViewModel.kt
+++ app/src/main/kotlin/dev/atick/compose/ui/home/HomeViewModel.kt
@@ -1,10 +1,12 @@
 package dev.atick.compose.ui.home
 
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dev.atick.compose.R
 import dev.atick.compose.repository.home.JetpackRepository
 import dev.atick.compose.ui.home.state.HomeUiState
+import dev.atick.core.ui.base.BaseViewModel
+import dev.atick.core.ui.utils.UiText
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -17,7 +19,7 @@ import kotlin.random.Random
 @HiltViewModel
 class HomeViewModel @Inject constructor(
     private val jetpackRepository: JetpackRepository
-) : ViewModel() {
+) : BaseViewModel<HomeUiState>() {
 
     private val _homeUiState = MutableStateFlow(HomeUiState())
     val homeUiState: StateFlow<HomeUiState> = _homeUiState.asStateFlow()
@@ -32,7 +34,12 @@ class HomeViewModel @Inject constructor(
             if (result.isSuccess) {
                 _homeUiState.update { it.copy(item = result.getOrNull(), loading = false) }
             } else {
-                _homeUiState.update { it.copy(error = "Error Loading Item", loading = false) }
+                _homeUiState.update {
+                    it.copy(
+                        error = UiText.StringResource(R.string.error_item_loading),
+                        loading = false
+                    )
+                }
             }
             getItemJob = null
         }
--- app/src/main/kotlin/dev/atick/compose/ui/home/state/HomeUiState.kt
+++ app/src/main/kotlin/dev/atick/compose/ui/home/state/HomeUiState.kt
@@ -1,9 +1,11 @@
 package dev.atick.compose.ui.home.state
 
 import dev.atick.compose.data.home.Item
+import dev.atick.core.ui.base.BaseUiState
+import dev.atick.core.ui.utils.UiText
 
 data class HomeUiState(
-    val item: Item? = null,
     val loading: Boolean = false,
-    val error: String? = null
-)
+    val error: UiText? = null,
+    val item: Item? = null
+) : BaseUiState()
