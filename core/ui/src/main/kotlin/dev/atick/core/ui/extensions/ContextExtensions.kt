e875062742d96843e7c790db418b0c4668e32850
Atick Faisal <atickfaisal@gmail.com>
2023-02-18 01:35:33 +0300

    Add file uri helper extensions

--- core/ui/src/main/kotlin/dev/atick/core/ui/extensions/ContextExtensions.kt
+++ core/ui/src/main/kotlin/dev/atick/core/ui/extensions/ContextExtensions.kt
@@ -4,12 +4,16 @@ import android.Manifest
 import android.app.Notification
 import android.content.Context
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
+import android.webkit.MimeTypeMap
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.annotation.RequiresPermission
 import androidx.core.app.NotificationManagerCompat
 import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import java.io.*
 
 fun Context.showToast(message: String) {
     Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
@@ -32,5 +36,57 @@ fun Context.showNotification(
 ) {
     with(NotificationManagerCompat.from(this)) {
         notify(notificationId, notification)
+    }
+}
+
+// ... https://medium.com/codex/how-to-implement-the-activity-result-api-takepicture-contract-with-uri-return-type-7c93881f5b0f
+fun Context.getTmpFileUri(appId: String): Uri {
+    val tmpFile = File.createTempFile("tmp_image_file", ".png", cacheDir).apply {
+        createNewFile()
+        deleteOnExit()
+    }
+
+    return FileProvider.getUriForFile(
+        applicationContext,
+        "${appId}.provider",
+        tmpFile
+    )
+}
+
+// ... https://stackoverflow.com/a/64488260/12737399
+fun Context.getFileFromContentUri(contentUri: Uri): File {
+    val fileExtension = getFileExtension(this, contentUri)
+    val fileName = "temp_file" + if (fileExtension != null) ".$fileExtension" else ""
+
+    val tempFile = File(cacheDir, fileName)
+    tempFile.createNewFile()
+
+    try {
+        val oStream = FileOutputStream(tempFile)
+        val inputStream = contentResolver.openInputStream(contentUri)
+
+        inputStream?.let {
+            copy(inputStream, oStream)
+        }
+
+        oStream.flush()
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+
+    return tempFile
+}
+
+private fun getFileExtension(context: Context, uri: Uri): String? {
+    val fileType: String? = context.contentResolver.getType(uri)
+    return MimeTypeMap.getSingleton().getExtensionFromMimeType(fileType)
+}
+
+@Throws(IOException::class)
+private fun copy(source: InputStream, target: OutputStream) {
+    val buf = ByteArray(8192)
+    var length: Int
+    while (source.read(buf).also { length = it } > 0) {
+        target.write(buf, 0, length)
     }
 }
