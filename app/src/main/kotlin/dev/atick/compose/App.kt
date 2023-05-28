7749138394a7fad24708bccc5c105cd4ebd93e78
Atick Faisal <atickfaisal@gmail.com>
2023-05-28 21:57:26 +0300

    Fix spotless check

--- app/src/main/kotlin/dev/atick/compose/App.kt
+++ app/src/main/kotlin/dev/atick/compose/App.kt
@@ -24,6 +24,6 @@ import timber.log.Timber
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
-        if(BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+        if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
     }
 }
