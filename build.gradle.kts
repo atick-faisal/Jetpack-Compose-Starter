dd28979e355340dc9de7e65f107744ef9a529973
Atick Faisal <atickfaisal@gmail.com>
2023-07-20 23:17:08 +0600

    Turn off gradle cache in actions

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ plugins {
     alias(libs.plugins.kotlin.serialization) apply(false)
     alias(libs.plugins.androidx.navigation.safeargs) apply(false)
     alias(libs.plugins.dagger.hilt.android) apply(false)
-    alias(libs.plugins.firebase.crashlytics) apply false
-    alias(libs.plugins.gms) apply false
+    alias(libs.plugins.firebase.crashlytics) apply(false)
+    alias(libs.plugins.gms) apply(false)
     alias(libs.plugins.dokka)
 }
