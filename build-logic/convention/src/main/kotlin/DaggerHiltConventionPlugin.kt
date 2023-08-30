cdafdf2f277111c2778301fb9081e097cf43c493
Atick Faisal <atickfaisal@gmail.com>
2023-08-31 01:13:40 +0300

    Migrate to dagger ksp

--- build-logic/convention/src/main/kotlin/DaggerHiltConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/DaggerHiltConventionPlugin.kt
@@ -1,10 +1,8 @@
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalogsExtension
-import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.getByType
-import org.jetbrains.kotlin.gradle.plugin.KaptExtension
 
 @Suppress("UNUSED")
 class DaggerHiltConventionPlugin : Plugin<Project> {
@@ -14,18 +12,21 @@ class DaggerHiltConventionPlugin : Plugin<Project> {
 
             with(pluginManager) {
                 apply("com.google.dagger.hilt.android")
-                apply("org.jetbrains.kotlin.kapt")
                 apply("com.google.devtools.ksp")
+                // Migrated to KSP
+                // apply("org.jetbrains.kotlin.kapt")
             }
 
             dependencies {
                 "implementation"(libs.findLibrary("dagger.hilt.android").get())
-                "kapt"(libs.findLibrary("dagger.hilt.compiler").get())
+                "ksp"(libs.findLibrary("dagger.hilt.compiler").get())
             }
 
+            /* Migrated to KSP
             configure<KaptExtension> {
                 correctErrorTypes = true
             }
+            */
         }
     }
 }
