982c0d3bf3689b9207ee6987ce605800e0176274
Atick Faisal <atickfaisal@gmail.com>
2023-09-12 15:30:58 +0300

    Fix default values

--- storage/preferences/src/main/kotlin/dev/atick/storage/preferences/model/Profile.kt
+++ storage/preferences/src/main/kotlin/dev/atick/storage/preferences/model/Profile.kt
@@ -7,13 +7,13 @@ import kotlinx.serialization.Serializable
  *
  * This data class is used for storing information about a user's profile.
  *
- * @property id The unique identifier for the profile. Defaults to "-1" if not provided.
- * @property name The name of the user. Defaults to "No Name" if not provided.
+ * @property id The unique identifier for the profile. Defaults to empty if not provided.
+ * @property name The name of the user. Defaults to empty if not provided.
  * @property profilePictureUriString The URI string for the user's profile picture, if available. Defaults to `null` if not provided.
  */
 @Serializable
 data class Profile(
-    val id: String = "-1",
-    val name: String = "No Name",
+    val id: String = String(),
+    val name: String = String(),
     val profilePictureUriString: String? = null,
 )
