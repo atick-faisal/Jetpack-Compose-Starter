8e2f0c719fd53ca31d5baaa342b304ff84398e2b
Atick Faisal <atickfaisal@gmail.com>
2024-05-25 20:42:39 +0300

    Replace collectWithLifeCycle with normal one

--- app/src/main/kotlin/dev/atick/compose/ui/profile/ProfileScreen.kt
+++ app/src/main/kotlin/dev/atick/compose/ui/profile/ProfileScreen.kt
@@ -25,6 +25,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -33,7 +34,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil.compose.AsyncImage
 import dev.atick.compose.R
 import dev.atick.compose.data.profile.ProfileScreenData
@@ -45,7 +45,7 @@ internal fun ProfileRoute(
     onShowSnackbar: suspend (String, String?) -> Boolean,
     profileViewModel: ProfileViewModel = hiltViewModel(),
 ) {
-    val profileState by profileViewModel.profileUiState.collectAsStateWithLifecycle()
+    val profileState by profileViewModel.profileUiState.collectAsState()
 
     StatefulComposable(
         state = profileState,
