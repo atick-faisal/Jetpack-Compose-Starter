667aa8fb932065e9267ec5b8a95277a70ebd1009
Atick Faisal <atickfaisal@gmail.com>
2025-02-02 12:34:50 +0300

    Rename PostEntity to JetpackEntity and update JetpackDao with new methods for managing jetpack data

--- storage/room/src/main/kotlin/dev/atick/storage/room/models/JetpackEntity.kt
+++ storage/room/src/main/kotlin/dev/atick/storage/room/models/JetpackEntity.kt
@@ -34,3 +34,38 @@ data class PostEntity(
     val url: String,
     val thumbnailUrl: String,
 )
+
+/**
+ * Represents a JetpackEntity, which is a data structure for storing information about a jetpack.
+ *
+ * @property id The unique identifier for the jetpack entity.
+ * @property name The name of the jetpack.
+ * @property price The price of the jetpack.
+ * @property lastSynced The timestamp of the last sync operation.
+ * @property needsSync A flag indicating whether the jetpack needs to be synced.
+ * @property isDeleted A flag indicating whether the jetpack has been deleted.
+ * @property syncAction The action to take when syncing the jetpack.
+ */
+@Entity(tableName = "jetpacks")
+data class JetpackEntity(
+    @PrimaryKey val id: String,
+    val name: String,
+    val price: Double,
+
+    // Sync metadata
+    val lastSynced: Long = 0,
+    val needsSync: Boolean = false,
+    val isDeleted: Boolean = false,
+    val syncAction: SyncAction = SyncAction.NONE,
+)
+
+/**
+ * Represents a SyncAction, which is an enumeration of the possible actions to take when syncing
+ * an item.
+ */
+enum class SyncAction {
+    NONE,
+    CREATE,
+    UPDATE,
+    DELETE,
+}
