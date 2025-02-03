18a40d4942b92771b98db3b9428d1b55f825bb6f
Atick Faisal <atickfaisal@gmail.com>
2025-02-03 18:30:19 +0300

    Add Firebase Auth module: implement authentication data source and Dagger integration

---
+++ firebase/auth/src/main/kotlin/dev/atick/firebase/auth/di/CredentialManagerModule.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2025 Atick Faisal
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.atick.firebase.auth.di
+
+import android.content.Context
+import androidx.credentials.CredentialManager
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+/**
+ * Dagger module for providing CredentialManager instance.
+ */
+@Module
+@InstallIn(SingletonComponent::class)
+object CredentialManagerModule {
+
+    /**
+     * Provides a singleton instance of CredentialManager.
+     *
+     * @param context The application context.
+     * @return CredentialManager instance.
+     */
+    @Provides
+    @Singleton
+    fun provideCredentialManager(@ApplicationContext context: Context): CredentialManager {
+        return CredentialManager.create(context)
+    }
+}
