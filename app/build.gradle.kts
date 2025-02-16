2ef2e5e18e1f12ccf9beeb37b1ff9385eca11262
Atick Faisal <atickfaisal@gmail.com>
2025-02-16 18:27:04 +0300

    Bump minor version to 1.1.0 in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ plugins {
 android {
     // ... Application Version ...
     val majorUpdateVersion = 1
-    val minorUpdateVersion = 0
+    val minorUpdateVersion = 1
     val patchVersion = 0
 
     val mVersionCode = majorUpdateVersion.times(10_000)
